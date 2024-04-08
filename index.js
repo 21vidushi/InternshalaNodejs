e34248c464bd9b04cf6d5a0f6c33cdd68208af41
Vidushi Singhal <singhalvidushi7417529406@gmail.com>
2024-04-08 14:54:43 +0530

    Files uploading

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app= express();
+const userRoute= require("./routes/userRoute");
+const courseRoute= require("./routes/courseRoute.js");
+const dbconnect= require("./database.js");
+const cookieParser = require("cookie-parser");
+const fileupload=require("express-fileupload");
+app.use(fileupload({
+    useTempFiles : true,
+    tempFileDir : '/tmp/'
+}));
+const cloudinary=require("./utils/upload.js");
+cloudinary.Cloudinaryconnect();
+dbconnect.dbconnect;
+require("dotenv").config();
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/v1/user",userRoute);
+app.use("/api/v1/course",courseRoute);
+app.listen(process.env.PORT,()=>{
+    console.log("Sever started");
+});
